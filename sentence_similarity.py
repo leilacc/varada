5077cccc9fe616e2398741a4a937c681553d2751
Leila Chan Currie <l.chancurrie@gmail.com>
2014-02-26 09:19:37 -0500

    Finished combining constituents of compound words in tagging

--- sentence_similarity.py
+++ sentence_similarity.py
@@ -4,6 +4,7 @@
 
 
 import nltk
+import string
 import util
 from nltk.corpus import wordnet as wn
 from nltk.corpus import wordnet_ic
@@ -54,18 +55,56 @@ def similarity(synset1, synset2, measure):
     return getattr(synset1, similarity_function)(synset2)
 
 def tag(sentence):
-  '''Tags a sentence with its parts of speech. Removes stop words.
+  '''Tags a sentence with its parts of speech. Combines compound words and
+  removes stop words.
 
   Args:
     sentence: A string. The sentence whose parts of speech will be tagged.
 
   Returns:
     The same sentence, but as a list of tuples with its parts of speech tagged.
-    Stop words are removed.
+    Compound words are tagged as one and stop words are removed.
     eg [('hello', 'UH'), ('how', 'WRB'), ('are', 'VBP'), ('you', 'PRP')]
   '''
   tagged_sentence = TAGGER.tag(nltk.word_tokenize(sentence))
+  print "Original tagged sentence:\n%s" % tagged_sentence
+  tagged_sentence = combine_compound_words(tagged_sentence, sentence)
   return remove_stopwords(tagged_sentence)
+
+def combine_compound_words(tagged_sentence, sentence):
+  '''Replaces individual words that make up a compound word in tagged_sentence
+  with the actual compound word.
+
+  Args:
+    tagged_sentence: A list of tuples of strings. Each tuple is of form
+      (word, tag)
+  
+  Returns:
+    The same tagged_sentence, but with constituent words of compound words
+    combined into a single compound word.
+  '''
+  compound_words = find_compound_words(sentence)
+  for c_word in compound_words:
+    i = c_word[0]
+    j = c_word[1]
+    synset = c_word[2]
+    constituent_words = ' '.join(split_into_words(sentence)[i:j + 1])
+    (tagged_sentence, k) = remove_individual_compound_words(constituent_words,
+                                                            tagged_sentence)
+    tagged_sentence.insert(k, (constituent_words, synset.pos))
+
+  return tagged_sentence
+
+def split_into_words(sentence):
+  '''Splits sentence into a list of words without punctuation.
+
+  Args:
+    sentence: A string
+
+  Returns:
+    A list of the words in sentence, with all punctuation removed.
+  '''
+  return "".join([i for i in sentence if i not in string.punctuation]).split()
 
 def find_compound_words(sentence):
   '''Identifies compound words in sentence.
@@ -74,17 +113,60 @@ def find_compound_words(sentence):
     sentence: A string.
     
   Returns:
-    TODO
+    A list of tuples of each compound word's start and end indices and synset
+    word, for all compound words found in sentence.
+    eg [(0, 1, Synset)] if a 2-word compound word starts the sentence.
   '''
+  compound_words = []
+  sentence = split_into_words(sentence)
+  
   # For each word, iteratively add on the words that follow to see if together
   # they form a compound word with a WordNet synset.
-  sentence = sentence.split()
   for i, word in enumerate(sentence):
     test_compound_word = word
     for j in range(i + 1, len(sentence)):
       test_compound_word += ('_' + sentence[j])
-      if wn.synsets(test_compound_word):
-        print wn.synsets(test_compound_word).pos
+      synsets = wn.synsets(test_compound_word)
+      if synsets:
+        for synset in synsets:
+          compound_words.append((i, j, synset))
+  
+  return compound_words
+
+def remove_individual_compound_words(constituent_words, tagged_sentence):
+  '''Removes the individual words in tagged_sentence that are constituent words
+  of a compound word.
+
+  Args:
+    constituent_words: A string of words that together form a compound word.
+    tagged_sentence: A list of tuples of strings. Each tuple is of form
+      (word, tag)
+
+  Returns:
+    A tuple. The first element is the tagged_sentence with the constituent
+    words removed, and the second element is the index in tagged_sentence
+    where the removal began.
+  '''
+  tagged_constituents = TAGGER.tag(nltk.word_tokenize(constituent_words))
+  cur_i = 0
+  start_i = False
+  end_i = 0
+  for i, tup in enumerate(tagged_sentence):
+    if tup == tagged_constituents[cur_i]:
+      cur_i += 1
+      if not start_i:
+        start_i = i
+    elif tup == tagged_constituents[0]:
+      start_i = i
+      cur_i = 1
+    else:
+      cur_i = 0
+
+    if cur_i == len(tagged_constituents):
+      end_i = i + 1
+      break
+
+  return (tagged_sentence[0:start_i] + tagged_sentence[end_i:], start_i)
 
 def remove_stopwords(tagged_sentence):
   '''Removes stopwords from a part-of-speech tagged_sentence.
@@ -305,6 +387,8 @@ if __name__ == '__main__':
  # candidate_source = util.load_pickle('candidate_source.dump')
  # compare_sentences(candidate_source['1'])
 
-  #print tag('He said, "hi!" by about')
-  find_compound_words('That red tape at the statue of liberty')
+  print ('\nFinal tagged sentence\n%s' %
+        tag('He said, "hi! red tape" by about statue of liberty'))
+#  print find_compound_words('That red tape at the statue of liberty')
+  #print find_compound_words('He said, "hi! red tape" by about statue of liberty')
   #print wn.synsets('red_tape')
